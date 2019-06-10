5fbf0264c789dd393df07c734bcec03f25ebd181
icedancemelody <49614958+icedancemelody@users.noreply.github.com>
2019-06-10 23:21:59 +0800

    Add files via upload

--- Helloworldscene.cpp
+++ Helloworldscene.cpp
@@ -1,20 +1,23 @@
+
 #include "HelloWorldScene.h"
 
 #include "SimpleAudioEngine.h"
-
-#include "Soider.h"
-
+#include "people.h"
+#include "tower.h"
+#include "equipment.h"
 #define TIMER 233
+
 USING_NS_CC;
+
 
 float sprite1_position_x;
 float sprite1_position_y;
 float x_touch;
 float y_touch;
 int countx, county;
-float flytime = 1;//子弹飞行时间
-int number_soider = 0;//第几波兵
-int attack_distance;//攻击距离
+int xiaobing_tag1=1000, xiaobing_tag2=1100, xiaobing_tag3=1210;
+
+
 Scene* HelloWorld::createScene()
 
 {
@@ -29,6 +32,37 @@ Scene* HelloWorld::createScene()
 
 }
 
+
+//设置一个自己在区域里移动的敌人
+void enemytick(Sprite *enemy)
+{
+	int enemymovetick = 0;
+	Point enemypoint = enemy->getPosition();
+	if (enemymovetick >= 0 && enemymovetick <= 20) {
+		enemymovetick++;
+		enemypoint.x += 2;
+	}
+	else if (enemymovetick > 20 && enemymovetick <= 60)
+	{
+		enemymovetick++;
+		enemypoint.x -= 2;
+
+	}
+	else if (enemymovetick > 60 && enemymovetick <= 80)
+	{
+		enemymovetick++;
+		enemypoint.x += 2;
+
+	}
+	else if (enemymovetick > 80)
+	{
+		enemymovetick = 0;
+
+
+	}
+	enemy->setPosition(enemypoint);
+
+}
 
 //地图随主角移动:获得主角在地图的相对位置，并设置地图位置。
 Point scenemove(Sprite *plane, TMXTiledMap *_tileMap,Vec2 vPos )
@@ -65,53 +99,15 @@ static void problemLoading(const char* filename)
 	
 }
 
-void HelloWorld::updatetime(float dt) 
-{
+
+void HelloWorld::updatetime(float dt) {
 	Vec2 origin = Director::getInstance()->getVisibleOrigin();
 	auto visibleSize = Director::getInstance()->getVisibleSize();
 	this->getChildByTag(TIMER)->removeFromParent();
 	time++;
-
+	
 	int minutes = time / 60;
 	int seconds = time - minutes * 60;
-	
-	if (seconds % 10 == 0)
-	{
-		auto enemy1 = Soider::createWithSoiderTypes(SoiderType1);
-		enemy1->setVelocity(Vec2(50, 0));
-		enemy1->setPosition(Vec2(400, 200));
-		enemy1->setFlipX(true);
-		this->addChild(enemy1, 2,number_soider*3+1);
-
-		auto enemy2 = Soider::createWithSoiderTypes(SoiderType1);
-		enemy2->setVelocity(Vec2(50, 0));
-		enemy2->setPosition(Vec2(300, 200));
-		enemy2->setFlipX(true);
-		this->addChild(enemy2, 2, number_soider * 3 + 2);
-
-		auto enemy3 = Soider::createWithSoiderTypes(SoiderType2);
-		enemy3->setVelocity(Vec2(50, 0));
-		enemy3->setPosition(Vec2(200, 200));
-		enemy3->setFlipX(true);
-		this->addChild(enemy3, 2, number_soider * 3 +3);
-
-		auto enemy4 = Soider::createWithSoiderTypes(SoiderType1);
-		enemy4->setVelocity(Vec2(-50, 0));
-		enemy4->setPosition(Vec2(1400, 200));
-		this->addChild(enemy4, 2, number_soider * 3 + 1001);
-
-		auto enemy5 = Soider::createWithSoiderTypes(SoiderType1);
-		enemy5->setVelocity(Vec2(-50, 0));
-		enemy5->setPosition(Vec2(1500, 200));
-		this->addChild(enemy5, 2, number_soider * 3 + 1002);
-
-		auto enemy6 = Soider::createWithSoiderTypes(SoiderType2);
-		enemy6->setVelocity(Vec2(-50, 0));
-		enemy6->setPosition(Vec2(1600, 200));
-		this->addChild(enemy6, 2, number_soider * 3 + 1003);
-
-		number_soider++;
-	}
 	std::string left = std::to_string(minutes);
 	std::string right = std::to_string(seconds);
 	if (minutes < 10) {
@@ -125,8 +121,6 @@ void HelloWorld::updatetime(float dt)
 		origin.y + visibleSize.height - timelabel->getContentSize().height));
 	this->addChild(timelabel, 2, TIMER);
 }
-
-
 bool HelloWorld::init()
 
 {
@@ -148,8 +142,7 @@ bool HelloWorld::init()
    Vec2 origin = Director::getInstance()->getVisibleOrigin();
 
    
-   this->schedule(schedule_selector(HelloWorld::updateenemy), (1.0f));
-   this->schedule(schedule_selector(HelloWorld::updatefriend), (1.0f));
+	
 
 	_tileMap = TMXTiledMap::create("bg_tiledMap.tmx");
 	_tileMap->setPosition(Vec2(origin.x,
@@ -165,23 +158,49 @@ bool HelloWorld::init()
 
 	
 	plane = Sprite::create("plane.png");
-	this->addChild(plane, 2,131);
+	_tileMap->addChild(plane, 2,103);
 	plane->setPosition(Vec2(a+100, b+40));
 	plane->setAnchorPoint(Vec2(1.0, 0.5));
 	
-     sprite2 = Sprite::create("plane1.png");
-	this->addChild(sprite2, 2,133);
-	sprite2->setPosition(Vec2(a+500, b+40));
-
-	time = 0;
-	auto timelabel = Label::createWithTTF("00:00", "fonts/Marker Felt.ttf", 30);
-	timelabel->setPosition(Vec2(origin.x + visibleSize.width - timelabel->getContentSize().width,
-		origin.y + visibleSize.height - timelabel->getContentSize().height));
-	this->addChild(timelabel, 2, TIMER);
-
-	this->schedule(schedule_selector(HelloWorld::updatetime), 1.0f);
+	hero plane(100, 100, 100, 100, 100, 100, 100, 100, 100, 100,0,0);
 	
+	log("%f", plane.get_blood());
 	
+	sprite2 = Sprite::create("plane1.png");
+	this->addChild(sprite2, 4);
+
+	sprite2->setPosition(Vec2(a+1000, b+40));
+	hero sprite2(10, 10, 10, 10, 10, 10, 10, 10, 10, 10,0,0);
+	log("%f", sprite2.get_blood());
+
+			   	
+	
+	//初始化敌人
+	auto enemy = Sprite::create("CloseSelected.png");
+	Size winSize = Director::getInstance()->getVisibleSize();
+	int enemymovetick = 0;
+
+	if (enemy == nullptr)
+
+	{
+
+		problemLoading("'CloseSelected.png'");
+
+	}
+
+	else
+
+	{
+
+		enemy->setPosition(Point(80,32));
+		enemy->setAnchorPoint(Point(0.5f, 0));
+		
+		this->addChild(enemy, 0);
+	
+		
+
+	}
+	enemytick(enemy);//没有成功实现
 
 	auto listenerkeyPad = EventListenerKeyboard::create();
 
@@ -213,12 +232,74 @@ bool HelloWorld::init()
 	
 	//scenemove(mapTild, plane, _tileMap);
 	
-		
 	
-
+	
+	time = 0;
+	auto timelabel = Label::createWithTTF("00:00", "fonts/Marker Felt.ttf", 30);
+	timelabel->setPosition(Vec2(origin.x + visibleSize.width - timelabel->getContentSize().width,
+		origin.y + visibleSize.height - timelabel->getContentSize().height));
+	this->addChild(timelabel, 2, TIMER);
+	
+	this->schedule(schedule_selector(HelloWorld::updatetime), 1.0f);
+	
 	return true;
 
+
 }
+
+
+
+/*
+//地图碰撞检测
+void playermaphcollision(Sprite *gameplayer)
+{
+	Point playerpoint = gameplayer->getPosition();
+	Size playersize = gameplayer->getContentSize();
+	TMXTiledMap* map = (TMXTiledMap*)getChildByTag(kTagTileMap);//地图参数kTagTileMap
+	int indexx, indexy;
+	char mch[256];
+	TMXLayer* layer = map->getLayer("logic");
+
+	//遍历图素块：for()
+	
+
+	//人物左右边界：
+
+	uo
+
+	//如果碰撞 设置主角位置（setPosition())
+
+
+
+}
+//竖直方向
+void playermaphcollision(Sprite *gameplayer)
+{
+}
+
+//更新函数update:
+void update(float dt,Sprite *gameplayer)
+{
+playermaphcollision();
+playermaphcollision();
+
+//设置主角位置
+Point playerpoint1=gameplayer->getPosition();
+playerpoint.y+=vmove;
+playerpoint.x+=1*hmove;
+enemytick();
+gameplayer->setPosition(playerpoint);
+
+地图随主角移动:获得主角在地图的相对位置，并设置地图位置。
+
+
+
+
+*/
+
+
+
+
 
 //判断精灵与精灵之间的碰撞
 bool isCircleCollision(Point pos1, float radius1, Point pos2, float radius2)
@@ -240,6 +321,23 @@ bool isinbound(Vec2 position, Vec2 touchposition,Vec2 size) {
 	return false;
 }
 
+void HelloWorld::myupdate(float dt)
+{
+	CCSprite* sp1 = (CCSprite*)this->getChildByTag(xiaobing_tag1); 
+	//获取 tag=101 的精灵
+	sp1->setPosition(sp1->getPosition() + ccp(1, 0));
+	
+
+	CCSprite* sp2 = (CCSprite*)this->getChildByTag(xiaobing_tag2);
+	//获取 tag=101 的精灵
+	sp2->setPosition(sp2->getPosition() + ccp(1, 0)); 
+	//每帧移动1
+	
+	CCSprite* sp3 = (CCSprite*)this->getChildByTag(xiaobing_tag3); //获取 tag=101 的精灵
+	sp3->setPosition(sp3->getPosition() + ccp(1, 0));
+	//每帧移动1
+	
+}
 
 
 
@@ -319,85 +417,26 @@ return ccp(fX, fY);
 }
 
 
-/*
-void HelloWorld::myupdate(float dt)
-{
-	CCSprite* sp1 = (CCSprite*)this->getChildByTag(101); //获取 tag=101 的精灵
-	sp1->setPosition(sp1->getPosition() + ccp(1, 0)); 
-	CCSprite* sp2 = (CCSprite*)this->getChildByTag(102); //获取 tag=101 的精灵
-	sp2->setPosition(sp2->getPosition() + ccp(1, 0));   //每帧移动1
-	CCSprite* sp3 = (CCSprite*)this->getChildByTag(103); //获取 tag=101 的精灵
-	sp3->setPosition(sp3->getPosition() + ccp(1, 0));   //每帧移动1
-}*/
-
-
-
-void HelloWorld::updatefriend(float dt)//实现友方AI
-{
-	for (int i1 = 1;i1 <= number_soider * 3;i1++)
-	{
-		Soider* friendsoider = (Soider*)this->getChildByTag(i1);
-		for (int j1 = 1001;j1 <= number_soider * 3 + 1000;j1++)
-		{
-			Soider* enemysoider = (Soider*)this->getChildByTag(j1);
-			if (i1 % 3)
-				attack_distance = 200;
-			else
-				attack_distance = 500;
-			if (ccpDistance(friendsoider->getPosition(), enemysoider->getPosition()) <= attack_distance)
-			{
-				friendsoider->setVelocity(Vec2(0, 0));
-				auto bullet2 = Sprite::create("CloseSelected.png");
-				this->addChild(bullet2, 2);
-				bullet2->setPosition(friendsoider->getPosition());
-				CCPoint attck_point = Vec2(enemysoider->getPosition().x, enemysoider->getPosition().y);
-				auto actionMove1 = CCMoveTo::create(flytime, attck_point);
-				auto actionMovehide1 = CCHide::create();
-
-				Sequence* sequence1 = Sequence::create(actionMove1, actionMovehide1, NULL);
-				bullet2->runAction(sequence1);
-				break;
-			}
-			else
-			{
-				friendsoider->setVelocity(Vec2(50, 0));
-			}
-		}
-	}
+void hero::be_attacked_by(hero other) {
+	_blood = _blood-(other.get_ad() - _hujia);
+	log("%f,%f,%f", _blood, other.get_ad(), _hujia);
+	
 }
-void HelloWorld::updateenemy(float dt)//实现敌军AI
-{
-	for (int i2 = 1001;i2 <= number_soider * 3+1000;i2++)
-	{
-		Soider* enemysoider1 = (Soider*)this->getChildByTag(i2);
-		for (int j2 = 1;j2 <= number_soider * 3 ;j2++)
-		{
-			Soider* friendsoider1 = (Soider*)this->getChildByTag(j2);
-			if ((i2-1000) % 3)
-				attack_distance = 200;
-			else
-				attack_distance = 500;
-			if (ccpDistance(enemysoider1->getPosition(), friendsoider1->getPosition()) <= attack_distance)
-			{
-				enemysoider1->setVelocity(Vec2(0, 0));
-				auto bullet3 = Sprite::create("CloseSelected.png");
-				this->addChild(bullet3, 2);
-				bullet3->setPosition(enemysoider1->getPosition());
-				CCPoint attck_point = Vec2(friendsoider1->getPosition().x, friendsoider1->getPosition().y);
-				auto actionMove2 = CCMoveTo::create(flytime, attck_point);
-				auto actionMovehide2 = CCHide::create();
 
-				Sequence* sequence2 = Sequence::create(actionMove2, actionMovehide2, NULL);
-				bullet3->runAction(sequence2);
-				break;
-			}
-			else
-			{
-				enemysoider1->setVelocity(Vec2(-50, 0));
-			}
-		}
-	}
+float hero::exp_up(hero other)
+{
+	_exp = _exp + other.get_exp();
+	return _exp;
 }
+
+
+
+hero _plane(100, 100, 100, 100, 100, 100, 100, 100, 100, 100,0,0);
+hero _sprite2(100, 10, 10, 10, 80, 10, 10, 10, 10, 10,1000,0);
+// 长按事件的update  计时器（没写）
+
+
+
 bool HelloWorld::touchBegan(Touch *touch, Event* event)
 {
 	x_touch = touch->getLocation().x + countx * 2;
@@ -408,36 +447,71 @@ bool HelloWorld::touchBegan(Touch *touch, Event* event)
 	Vec2 sprite2_size = sprite2->getContentSize();
 	log("%f,%f", touchposition.x, touchposition.y);
 	log("%f,%f", sprite2_size.x, sprite2_size.y);
+
 	if (isinbound(sprite2_position, touchposition, sprite2_size)) {
 		log("6666");
-		
 		auto bullet1 = Sprite::create("CloseNormal.png");
-		this->addChild(bullet1,2);
+		this->addChild(bullet1,0);
+	    bullet1->setPosition(ccp(sprite1_position_x, sprite1_position_y));
+		//兵线
+		auto xiaobing1 = Sprite::create("boss.png");
+		this->addChild(xiaobing1, 1, xiaobing_tag1);
+		xiaobing1->setScale(0.3);
 
-		bullet1->setPosition(ccp(sprite1_position_x, sprite1_position_y));
+		auto xiaobing2 = Sprite::create("boss.png");
+		this->addChild(xiaobing2, 1, xiaobing_tag2);
+		xiaobing2->setScale(0.3);
+
+		auto xiaobing3 = Sprite::create("boss.png");
+		this->addChild(xiaobing3, 1, xiaobing_tag3);
+		xiaobing3->setScale(0.3);
+
+		xiaobing1->setPosition(ccp(300, 200));
+		xiaobing2->setPosition(ccp(200, 200));
+		xiaobing3->setPosition(ccp(100, 200));
+		this->schedule(schedule_selector(HelloWorld::myupdate));
+		
+
 
 
 		CCPoint ptOutOfWorld1 = Vec2(x_touch, y_touch);
-		float v = 300.000000;
+		float v = 3000.000000;
 		//传说中的定时器
 		//this->scheduleUpdate();
 		//this->myupdate(x_touch - sprite1_position_x, y_touch - sprite1_position_y, bullet1);
-		/*this->schedule(schedule_selector(HelloWorld::myupdate));*/
+	
 		
-
-		auto actionMove = CCMoveTo::create(flytime, ptOutOfWorld1);
+		float time = sqrt((x_touch - sprite1_position_x)*(x_touch - sprite1_position_x) + (y_touch - sprite1_position_y)*(y_touch - sprite1_position_y)) / v;
+		auto actionMove = CCMoveTo::create(time, ptOutOfWorld1);
 		auto actionMovehide = CCHide::create();
 
 		
-			Sequence* sequence = Sequence::create(actionMove, actionMovehide, NULL);
+		Sequence* sequence = Sequence::create(actionMove, actionMovehide, NULL);
 		bullet1->runAction(sequence);//发射子弹
 
-		/*//子弹与精灵碰撞消除子弹
+		//子弹与精灵碰撞消除子弹
 		if (bullet1->boundingBox().intersectsRect(sprite2->boundingBox()))
 		{
 			bullet1->setScale(2.0);
-		};*/
+		};
 
+	
+
+		_sprite2.be_attacked_by(_plane);
+	
+		log("%f", _sprite2.get_blood());
+		if (_sprite2.get_blood() <= 0) {
+		
+
+			sprite2->setVisible(false);
+			int x = _plane.exp_up(_sprite2);
+			_plane.judge_level_up(x);
+
+		
+
+		}
+
+		
 
 	}
 
@@ -459,7 +533,6 @@ bool HelloWorld::touchBegan(Touch *touch, Event* event)
 	return true;
 
 };
-
 
 
 
@@ -537,19 +610,7 @@ void HelloWorld::touchEnded(Touch *touch, Event* event)
 };
 
 
- /*void HelloWorld::update(float dt)
-{
-	
-	CCMoveBy *moveby = CCMoveBy::create(0.1f, ccp(2, 0));
 
-	CCActionInterval *SineOUt = CCEaseSineInOut::create(moveby);
-	sprite1_position_x = plane->getPositionX() + 20;
-	sprite1_position_y = plane->getPositionY();
-	plane->runAction(moveby);
-	Vec2 sprite1_position = plane->getPosition();
-	Vec2 move = scenemove(plane, _tileMap, sprite1_position);
-	this->setPosition(move);
-}*/
 
 bool HelloWorld::isKeyPressed(EventKeyboard::KeyCode keyCode) {
 	if (keys[keyCode]) {
@@ -623,7 +684,7 @@ void HelloWorld::update(float delta) {
 		this->setPosition(move);
 	}
 
-	//log("update");
+	
 }
 
 /*void HelloWorld::onKeyPressed(EventKeyboard::KeyCode keyCode, cocos2d::Event *event) {
