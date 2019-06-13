2ce0d20889e73d2702aedc0ba3551c27e6b1940c
Pluto6666 <51647098+Pluto6666@users.noreply.github.com>
2019-06-13 21:36:52 +0800

    已更新装备界面和记分板界面

--- GameMap.cpp
+++ GameMap.cpp
@@ -49,12 +49,14 @@ bool GameMap::init() {
 		animation->addSpriteFrameWithFile(file_name);
 	}
 	animation->setRestoreOriginalFrame(true);
-	animation->setDelayPerUnit(2.0f / 10.0f);
+	animation->setDelayPerUnit(2.0f / 20.0f);
 	animation->setLoops(-1);
 	Animate* animate = Animate::create(animation);
 	sprite->runAction(animate);
 	
-	//设置人物血条
+	//设置人物血条(血条层)
+	
+	_HPLayer = Layer::create();
 	m_pBloodView = new bloodView();
 	m_pBloodView->setPosition(Vec2(130,550));//位置（可自己调整）
 	m_pBloodView->setScale(2.2f);//缩放比例
@@ -64,86 +66,45 @@ bool GameMap::init() {
 	m_pBloodView->setCurrentBlood(100.0f);//从满血开始
 	Sprite *xuekuang= Sprite::create("/sprite/kuang.png");//血量框
 	xuekuang->setPosition(Vec2(m_pBloodView->getPositionX(), m_pBloodView->getPositionY()));
-	this->addChild(xuekuang, 2);
-	this->addChild(m_pBloodView, 2);
+	_HPLayer->addChild(xuekuang, 2);
+	_HPLayer->addChild(m_pBloodView, 2);
+	this->addChild(_HPLayer, 2,JCHM);
 	
+	//设置进入记分板按钮
+	auto ScoreItem = MenuItemImage::create(
+		"/Button/scoreboard.png",
+		"/Button/scoreboard.png",
+	CC_CALLBACK_1(GameMap::createScoreCallBack,this)
+	);
+	float board_x= origin.x + ScoreItem->getContentSize().width / 2+10;
+	float board_y= origin.y + visibleSize.height / 2 - ScoreItem->getContentSize().height / 2 - 60;
+	ScoreItem->setPosition(Vec2(board_x, board_y));
+		
 	
+	//设置进入商店按钮
 	auto shopItem= MenuItemImage::create(
-		"/Button/shopbutton.png",
-		"/Button/shopbutton.png",
+		"/Button/shopButton666.png",
+		"/Button/shopButton666.png",
 		CC_CALLBACK_1(GameMap::createShopCallBack, this));
 	float spmn_x = origin.x + shopItem->getContentSize().width / 2;
-	float spmn_y = origin.y + visibleSize.height / 2 - shopItem->getContentSize().height / 2;
+	float spmn_y = origin.y + visibleSize.height / 2 - shopItem->getContentSize().height / 2+30;
 	shopItem->setPosition(Vec2(spmn_x, spmn_y));
 
-	auto menu = Menu::create(shopItem, NULL);
+	auto menu = Menu::create(shopItem, ScoreItem,NULL);
 	menu->setPosition(Vec2::ZERO);
 	this->addChild(menu, 5,333);
 
 	time = 0;
+	_Timelabel = Layer::create();
 	auto timelabel = Label::createWithTTF("00:00", "fonts/Marker Felt.ttf", 30);
 	timelabel->setPosition(Vec2(origin.x + visibleSize.width-timelabel->getContentSize().width,
 		origin.y + visibleSize.height - timelabel->getContentSize().height));
-	this->addChild(timelabel, 2,TIMER);
+	_Timelabel->addChild(timelabel, 2, TIMER);
 	this->scheduleUpdate();
 	this->schedule(schedule_selector(GameMap::updatetime), 1.0f);
-	
+	this->addChild(_Timelabel,2,JCHM);
 	return true;
 }
-
-
-/*void GameMap::initBG() {
-	auto visibleSize = Director::getInstance()->getVisibleSize();
-	//添加背景地图
-	auto bg = TMXTiledMap::create("/pd_tilemap.tmx/");
-	this->addChild(bg, -2, JCHM);
-
-	//添加发光背景
-
-	//添加背景精灵1
-
-
-
-}
-
-void GameMap::onExit() {
-	Layer::onExit();
-	//停止调用射子弹函数
-	//注销事件监听器
-
-	auto nodes = this->getChildren();
-	for (const auto&node : nodes) {
-		if (node->getTag() != JCHM) {
-			this->removeChild(node);
-		}
-	}
-}
-
-void GameMap::onEnter() {
-	Layer::onEnter();
-	Size visibleSize = Director::getInstance()->getVisibleSize();
-	auto pauseSprite = Sprite::createWithSpriteFrameName("");
-	auto pauseMenuItem = MenuItemSprite::create(, , CC_CALLBACK_1(GameMap::menuPauseCallBack, this));
-	auto pauseMenu = Menu::create(pauseMenuItem, NULL);
-	pauseMenu->setPosition(Vec2(30, visibleSize.height - 28));
-	this->addChild(pauseMenu, 20, 999);
-}
-
-void GameMap::menuPauseCallBack(cocos2d::Ref* pSender) {
-	log("menuPauseCallBack");
-	this->pause();
-	for (const auto&node : this->getChildren()) {
-		node->pause();
-		}
-	Size visibleSize = Director::getInstance()->getVisibleSize();
-	
-	//返回主菜单
-	auto backNormal = Sprite::createWithSpriteFrameName("");
-	auto backSelected = Sprite::createWithSpriteFrameName("");
-	auto backMenuItem = MenuItemSprite::create(backNormal, backSelected, CC_CALLBACK_1(GameMap::menuBackCallback, this));
-
-
-}*/
 
 //商店的创建
 void GameMap::createShopCallBack(cocos2d::Ref* pSender) {
@@ -163,29 +124,179 @@ void GameMap::createShopCallBack(cocos2d::Ref* pSender) {
 		"/Button/shopButtonSelected.png",
 		CC_CALLBACK_1(GameMap::closeShopCallBack, this)
 	);
-	closeShopButton->setPosition(_shopBg->getPosition() + Vec2(320, -140));
+	closeShopButton->setPosition(_shopBg->getPosition() + Vec2(375, -180));
 	
 	//item
-	auto itemCloth = MenuItemImage::create(
-		"/equipment/cloth.png",
-		"/equipment/cloth.png",
-		CC_CALLBACK_1(GameMap::buyItemCallBack, this)
-	);
-	itemCloth->setPosition(_shopBg->getPosition() + Vec2(-100, -50));
+	MenuItemImage* ItemSword[4];
+	MenuItemImage* ItemCloth[4];
+	MenuItemImage* ItemLive[4];
+	MenuItemImage* ItemShoe[4];
+	for (int i = 1; i < 5; i++) {
+		char fileName[50] = {0};
+		char callbackname[50] = {0};
+		std::string money;
+		std::string dscb;
+		if (i == 1) { 
+			money = "300g"; 
+			dscb = "Attack+10";
+		}
+		if (i == 2) { 
+			money = "700g"; 
+			dscb = "Attack+50";
+		}
+		if (i == 3) { 
+			money = "1300g"; 
+			dscb = "Attack+100";
+		}
+		if (i == 4) { 
+			money = "2000g"; 
+			dscb = "Attack+150";
+		}
+		sprintf(fileName, "/equipment/sword%d.png", i);
+		ItemSword[i-1] = MenuItemImage::create(
+			fileName,
+			fileName,
+			CC_CALLBACK_1(GameMap::buyItemCallBack, this)
+		);
+		ItemSword[i - 1]->setScale(0.8f);
+		ItemSword[i-1]->setPosition(_shopBg->getPosition() + Vec2(-300, 220 - (80 * i)));
+		auto money_label = Label::createWithSystemFont(money, "fonts/arial-unicode-26.fnt", 22);
+		money_label->setPosition(_shopBg->getPosition() + Vec2(-360, 220 - (80 * i)));
+		money_label->setColor(Color3B::BLACK);
+		_shopLayer->addChild(money_label, 2);
+		auto dscb_label = Label::createWithSystemFont(dscb, "fonts/arial-unicode-26.fnt", 15);
+		dscb_label->setPosition(_shopBg->getPosition() + Vec2(-260, 180 - (80 * i)));
+		dscb_label->setColor(Color3B::RED);
+		_shopLayer->addChild(dscb_label, 2);
+	};
+	for (int i = 1; i < 5; i++) {
+		char fileName[50] = { 0 };
+		char callbackname[50] = { 0 };
+		sprintf(fileName, "/equipment/cloth%d.png", i);
+		std::string money;
+		std::string dscb;
+		if (i == 1) { 
+			money = "450g"; 
+			dscb = "Armor+10";
+		}
+		if (i == 2) { 
+			money = "1000g"; 
+			dscb = "Armor+50";
+		}
+		if (i == 3) { 
+			money = "1500g"; 
+			dscb = "Armor+100";
+		}
+		if (i == 4) { 
+			money = "2400g"; 
+			dscb = "Armor+150";
+		}
+		ItemCloth[i - 1] = MenuItemImage::create(
+			fileName,
+			fileName,
+			CC_CALLBACK_1(GameMap::buyItemCallBack, this)
+		);
+		ItemCloth[i - 1]->setScale(0.8f);
+		ItemCloth[i - 1]->setPosition(_shopBg->getPosition() + Vec2(-120, 220 - (80 * i)));
+		auto money_label = Label::createWithSystemFont(money, "fonts/arial-unicode-26.fnt", 22);
+		money_label->setPosition(_shopBg->getPosition() + Vec2(-180, 220 - (80 * i)));
+		money_label->setColor(Color3B::BLACK);
+		_shopLayer->addChild(money_label, 2);
+		auto dscb_label = Label::createWithSystemFont(dscb, "fonts/arial-unicode-26.fnt", 15);
+		dscb_label->setPosition(_shopBg->getPosition() + Vec2(-50, 180 - (80 * i)));
+		dscb_label->setColor(Color3B::RED);
+		_shopLayer->addChild(dscb_label, 2);
+	};
+	for (int i = 1; i < 5; i++) {
+		char fileName[50] = { 0 };
+		char callbackname[50] = { 0 };
+		sprintf(fileName, "/equipment/live%d.png", i);
+		std::string money;
+		std::string dscb;
+		if (i == 1) { 
+			money = "400g"; 
+			dscb = "Blood+200";
+		}
+		if (i == 2) {
+			money = "800g"; 
+			dscb = "Blood+500";
+		}
+		if (i == 3) { 
+			money = "1200g"; 
+			dscb = "Blood+1200";
+		}
+		if (i == 4) { 
+			money = "1600g"; 
+			dscb = "Now Blood Double!!";
+		}
+		ItemLive[i - 1] = MenuItemImage::create(
+			fileName,
+			fileName,
+			CC_CALLBACK_1(GameMap::buyItemCallBack, this)
+		);
+		ItemLive[i - 1]->setScale(0.8f);
+		ItemLive[i - 1]->setPosition(_shopBg->getPosition() + Vec2(120, 220 - (80 * i)));
+		auto money_label = Label::createWithSystemFont(money, "fonts/arial-unicode-26.fnt", 22);
+		money_label->setPosition(_shopBg->getPosition() + Vec2(60, 220 - (80 * i)));
+		money_label->setColor(Color3B::BLACK);
+		_shopLayer->addChild(money_label, 2);
+		auto dscb_label = Label::createWithSystemFont(dscb, "fonts/arial-unicode-26.fnt", 15);
+		dscb_label->setPosition(_shopBg->getPosition() + Vec2(160, 180 - (80 * i)));
+		dscb_label->setColor(Color3B::RED);
+		_shopLayer->addChild(dscb_label, 2);
+	};
+	for (int i = 1; i < 5; i++) {
+		char fileName[50] = { 0 };
+		char callbackname[50] = { 0 };
+		sprintf(fileName, "/equipment/shoe%d.png", i);
+		std::string money;
+		std::string dscb;
+		if (i == 1) { 
+			money = "250g";
+			dscb = "Speed+25";
+		}
+		if (i == 2) { 
+			money = "800g"; 
+			dscb = "Speed+80";
+		}
+		if (i == 3) { 
+			money = "1100g"; 
+			dscb = "Speed+50&Attack+80";
+		}
+		if (i == 4) { 
+			money = "1100g"; 
+			dscb = "Speed+50&Armor+80";
+		}
+		ItemShoe[i - 1] = MenuItemImage::create(
+			fileName,
+			fileName,
+			CC_CALLBACK_1(GameMap::buyItemCallBack, this)
+		);
+		ItemShoe[i - 1]->setScale(0.64f);
+		ItemShoe[i - 1]->setPosition(_shopBg->getPosition() + Vec2(300, 220 - (80 * i)));
+		auto money_label = Label::createWithSystemFont(money, "fonts/arial-unicode-26.fnt", 22);
+		money_label->setPosition(_shopBg->getPosition() + Vec2(240, 220 - (80 * i)));
+		money_label->setColor(Color3B::BLACK);
+		_shopLayer->addChild(money_label, 2);
+		auto dscb_label = Label::createWithSystemFont(dscb, "fonts/arial-unicode-26.fnt", 15);
+		dscb_label->setPosition(_shopBg->getPosition() + Vec2(330, 180 - (80 * i)));
+		dscb_label->setColor(Color3B::RED);
+		_shopLayer->addChild(dscb_label, 2);
+	};
 
-	auto itemSword = MenuItemImage::create(
-		"/equipment/sword.png",
-		"/equipment/sword.png",
-		CC_CALLBACK_1(GameMap::buyItemCallBack, this)
-	);
-	itemSword->setPosition(_shopBg->getPosition() + Vec2(100, -50));
+
 	
 	//shopshow
 	Sprite* shopsprite = Sprite::create("shop.png");
-	shopsprite->setPosition(_shopBg->getPosition() + Vec2(0, 100));
+	shopsprite->setPosition(_shopBg->getPosition() + Vec2(0, 190));
 	_shopLayer->addChild(shopsprite, 2);
 	//menucreate
-	auto mn = Menu::create(closeShopButton,itemCloth ,itemSword,NULL);
+	auto mn = Menu::create(closeShopButton,
+		ItemSword[0], ItemSword[1], ItemSword[2], ItemSword[3],
+		ItemCloth[0], ItemCloth[1], ItemCloth[2], ItemCloth[3],
+		ItemLive[0], ItemLive[1], ItemLive[2], ItemLive[3],
+		ItemShoe[0], ItemShoe[1], ItemShoe[2], ItemShoe[3],
+		NULL);
 	mn->setPosition(Vec2::ZERO);
 	_shopLayer->addChild(mn, 6);
 	_shopLayer->addChild(_shopBg);
@@ -196,14 +307,55 @@ void GameMap::createShopCallBack(cocos2d::Ref* pSender) {
 void GameMap::closeShopCallBack(cocos2d::Ref* pSender) {
 	_shopLayer->removeFromParent();
 }
-void GameMap::buyItemCallBack(cocos2d::Ref* pSender){
-	
-}
+//由此开始是16种装备购买的回调函数
+void GameMap::buyItemCallBack(cocos2d::Ref* pSender){}
 
+
+void GameMap::buySwordCallBack1(cocos2d::Ref* pSender){
+	//if(hero->money()<300||eqp_num==6||hero->havesword[0]==true){return;}            不能买条件：钱不够、装备满、已经1
+	//hero->get.ad()+=10;                加攻击
+	//bool 函数类
+	//hero->havesword[0]=true;     设定英雄已有此装备
+	//hero->eqp_num++;              装备栏数量+1
+}
+void buySwordCallBack2(cocos2d::Ref* pSender){
+	//if(hero->money()<700||hero->havesword[0]==false||hero->havesword[1]==true){return;}                  不能买条件：钱不够、没有1、已有2
+	//hero->havesword[0]=false;         设定英雄没有此装备了
+	//hero->get.ad()+=（50-10）;                    加攻击
+	//hero->havesword[1]=true;          设定英雄已有此装备
+}
+void buySwordCallBack3(cocos2d::Ref* pSender){
+	//if(hero->money()<1300||hero->havesword[1]==false||hero->havesword[2]==true){return;}                  不能买条件：钱不够、没有2、已有3
+	//hero->havesword[1]=false;         设定英雄没有此装备了
+	//hero->get.ad()+=（100-50）;                    加攻击
+	//hero->havesword[2]=true;          设定英雄已有此装备
+}
+void buySwordCallBack4(cocos2d::Ref* pSender){
+	//if(hero->money()<2000||hero->havesword[2]==false||hero->havesword[3]==true){return;}                  不能买条件：钱不够、没有3、已有4
+	//hero->havesword[2]=false;         设定英雄没有此装备了
+	//hero->get.ad()+=（150-100）;                    加攻击
+	//hero->havesword[3]=true;          设定英雄已有此装备
+}
+void buyClothCallBack1(cocos2d::Ref* pSender){}
+void buyClothCallBack2(cocos2d::Ref* pSender){}
+void buyClothCallBack3(cocos2d::Ref* pSender){}
+void buyClothCallBack4(cocos2d::Ref* pSender){}
+void buyLiveCallBack1(cocos2d::Ref* pSender){}
+void buyLiveCallBack2(cocos2d::Ref* pSender){}
+void buyLiveCallBack3(cocos2d::Ref* pSender){}
+void buyLiveCallBack4(cocos2d::Ref* pSender){}
+void buyShoeCallBack1(cocos2d::Ref* pSender){}
+void buyShoeCallBack2(cocos2d::Ref* pSender){}
+void buyShoeCallBack3(cocos2d::Ref* pSender){}
+void buyShoeCallBack4(cocos2d::Ref* pSender){}
+
+
+
+//由此结束装备购买函数
 void GameMap::updatetime(float dt) {
 	Vec2 origin = Director::getInstance()->getVisibleOrigin();
 	auto visibleSize = Director::getInstance()->getVisibleSize();
-	this->getChildByTag(TIMER)->removeFromParent();
+	_Timelabel->getChildByTag(TIMER)->removeFromParent();
 	time++;
 	int minutes = time / 60;
 	int seconds = time - minutes * 60;
@@ -218,7 +370,74 @@ void GameMap::updatetime(float dt) {
 	auto timelabel = Label::createWithTTF(left+":"+right, "fonts/Marker Felt.ttf", 30);
 	timelabel->setPosition(Vec2(origin.x + visibleSize.width - timelabel->getContentSize().width,
 		origin.y + visibleSize.height  - timelabel->getContentSize().height));
-	this->addChild(timelabel, 2,TIMER);
+	_Timelabel->addChild(timelabel, 2,TIMER);
 	m_pBloodView->setCurrentBlood(m_pBloodView->getCurrentBlood() - 1);
 }
 
+
+void GameMap::createScoreCallBack(cocos2d::Ref* pSender){
+	auto visibleSize = Director::getInstance()->getVisibleSize();
+	//Scoreboardbg
+	_ScoreboardLayer = Layer::create();
+	auto _Bg = Sprite::create("/BG/scoreboardbg.jpg");
+	_Bg->setPosition(Vec2(
+		visibleSize.width / 2,
+		visibleSize.height / 2
+	));
+
+	std::string a = std::to_string(time);
+
+
+	//closebutton
+	auto closeScoreButton = MenuItemImage::create(
+		"/Button/closescore.png",
+		"/Button/closescore.png",
+		CC_CALLBACK_1(GameMap::closeScoreCallBack, this)
+	);
+	closeScoreButton->setPosition(_Bg->getPosition() + Vec2(260, 195));
+
+	//我方战绩（包括英雄类型、击杀数、死亡数、金钱）
+	auto my = Label::createWithSystemFont("MY HERO:warrior  LV:"+a,"fonts/arial-unicode-26.fnt",26);//间隔10字符位
+	my->setPosition(Vec2(visibleSize.width/2,visibleSize.height/2+180));
+	_ScoreboardLayer->addChild(my, 4);
+
+	auto my_kill = Label::createWithSystemFont("KILL:"+a, "fonts/arial-unicode-26.fnt", 26);
+	my_kill->setPosition(Vec2(visibleSize.width / 2, visibleSize.height / 2 + 150));
+	_ScoreboardLayer->addChild(my_kill, 4);
+
+	auto my_die = Label::createWithSystemFont("DIE:"+a, "fonts/arial-unicode-26.fnt", 26);
+	my_die->setPosition(Vec2(visibleSize.width / 2, visibleSize.height / 2 + 120));
+	_ScoreboardLayer->addChild(my_die, 4);
+
+	auto my_money = Label::createWithSystemFont("MONEY:"+a, "fonts/arial-unicode-26.fnt", 26);
+	my_money->setPosition(Vec2(visibleSize.width / 2, visibleSize.height / 2 + 90));
+	_ScoreboardLayer->addChild(my_money, 4);
+
+	//敌方战绩（包括英雄类型、击杀数、死亡数、金钱）
+	auto other = Label::createWithSystemFont("OTHER HERO:mega  LV", "fonts/arial-unicode-26.fnt", 26);//间隔10字符位
+	other->setPosition(Vec2(visibleSize.width / 2, visibleSize.height / 2 -90));
+	_ScoreboardLayer->addChild(other, 4);
+
+	auto other_kill = Label::createWithSystemFont("KILL:" + a, "fonts/arial-unicode-26.fnt", 26);
+	other_kill->setPosition(Vec2(visibleSize.width / 2, visibleSize.height / 2 -120));
+	_ScoreboardLayer->addChild(other_kill, 4);
+
+	auto other_die = Label::createWithSystemFont("DIE:" + a, "fonts/arial-unicode-26.fnt", 26);
+	other_die->setPosition(Vec2(visibleSize.width / 2, visibleSize.height / 2 -150));
+	_ScoreboardLayer->addChild(other_die, 4);
+
+	auto other_money = Label::createWithSystemFont("MONEY:" + a, "fonts/arial-unicode-26.fnt", 26);
+	other_money->setPosition(Vec2(visibleSize.width / 2, visibleSize.height / 2 -180));
+	_ScoreboardLayer->addChild(other_money, 4);
+
+
+	auto mn = Menu::create(closeScoreButton, NULL);
+	mn->setPosition(Vec2::ZERO);
+	_ScoreboardLayer->addChild(mn, 6);
+	_ScoreboardLayer->addChild(_Bg);
+	this->addChild(_ScoreboardLayer, 5, JCHM);
+}
+
+void GameMap::closeScoreCallBack(cocos2d::Ref* pSender) {
+	_ScoreboardLayer->removeFromParent();
+}
--- GameMap.h
+++ GameMap.h
@@ -7,20 +7,38 @@ class GameMap : public cocos2d::Layer
 {
 	cocos2d::TMXTiledMap* _tileMap;
 	cocos2d::Layer* _shopLayer;
+	cocos2d::Layer* _Timelabel;
+	cocos2d::Layer* _HPLayer;
+	cocos2d::Layer* _ScoreboardLayer;
 	public:
 	static cocos2d::Scene* createScene();
 
 	int time=0;
 	virtual bool init();
 	bloodView *m_pBloodView;//血条类
-	/*void initBG();
-	void onExit();
-	void onEnter();
-	void menuPauseCallBack(cocos2d::Ref* pSender);*/
 	void createShopCallBack(cocos2d::Ref* pSender);
 	void closeShopCallBack(cocos2d::Ref* pSender);
-	void buyItemCallBack(cocos2d::Ref* pSender);
+	void createScoreCallBack(cocos2d::Ref* pSender);
+	void closeScoreCallBack(cocos2d::Ref* pSender);
 	void updatetime(float dt);
+	//装备购买16种函数
+	void buyItemCallBack(cocos2d::Ref* pSender);
+	void buySwordCallBack1(cocos2d::Ref* pSender);
+	void buySwordCallBack2(cocos2d::Ref* pSender);
+	void buySwordCallBack3(cocos2d::Ref* pSender);
+	void buySwordCallBack4(cocos2d::Ref* pSender);
+	void buyClothCallBack1(cocos2d::Ref* pSender);
+	void buyClothCallBack2(cocos2d::Ref* pSender);
+	void buyClothCallBack3(cocos2d::Ref* pSender);
+	void buyClothCallBack4(cocos2d::Ref* pSender);
+	void buyLiveCallBack1(cocos2d::Ref* pSender);
+	void buyLiveCallBack2(cocos2d::Ref* pSender);
+	void buyLiveCallBack3(cocos2d::Ref* pSender);
+	void buyLiveCallBack4(cocos2d::Ref* pSender);
+	void buyShoeCallBack1(cocos2d::Ref* pSender);
+	void buyShoeCallBack2(cocos2d::Ref* pSender);
+	void buyShoeCallBack3(cocos2d::Ref* pSender);
+	void buyShoeCallBack4(cocos2d::Ref* pSender);
 	// implement the "static create()" method manually
 	CREATE_FUNC(GameMap);
 };
